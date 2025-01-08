c692c9f526acf851774da885713abf2ce4785421
binetougaye <fabigaye3@gmail.com>
2025-01-08 15:10:49 +0000

    feat: validations des champs

--- public/app.js
+++ public/app.js
@@ -16,37 +16,49 @@ const emailError = document.getElementById("emailError")
 const checkError = document.getElementById("checkError")
 const messageError = document.getElementById("messageError")
 const consentError = document.getElementById("consentError")
+// < i class="fa-solid fa-circle-exclamation" ></ i>
+let validEmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 function validateForm(e) {
-
-
-
     e.preventDefault()
-
     let isValid = true
     // Validations des champs
     if (firstName.value.trim() === "") {
-        firstNameError.textContent = "The first name field is required"
+        firstNameError.innerHTML = "The first name field is required"
+        firstName.classList.add("input")
         isValid = false
     }
     if (lastName.value.trim() === "") {
         lastNameError.textContent = "The last name field is required"
+        lastName.classList.add("input")
         isValid = false
     }
     if (emailAdress.value.trim() === "") {
         emailError.textContent = "The email field is required"
+        emailAdress.classList.add("input")
         isValid = false
     }
+    if (!(emailAdress.value.match(validEmailRegex))) {
+        emailError.textContent = "Please enter a valid email format"
+        emailAdress.classList.add("input")
+        isValid = false
+
+
+    }
+
     if (!(check1.checked && check2.checked)) {
         checkError.textContent = "Please select both checkboxes"
+
         isValid = false
     }
     if (message.value.trim() === "") {
         messageError.textContent = "Message is required"
+        message.classList.add("input")
         isValid = false
     }
     if (!consentCheck.checked) {
         consentError.textContent = "You must agree to the terms"
+
         isValid = false
     }
     if (isValid) {
@@ -56,6 +68,11 @@ function validateForm(e) {
         checkError.textContent = "";
         messageError.textContent = "";
         consentError.textContent = "";
+        firstName.classList.remove("input")
+        lastName.classList.remove("input")
+        emailAdress.classList.remove("input")
+        message.classList.remove("input")
+        form.reset()
         showToast()
     }
 
@@ -66,9 +83,9 @@ function validateForm(e) {
 function showToast() {
     Toastify({
         text: "Formulaire soumis avec succès!",
-        duration: 3000, // Durée en millisecondes
+        duration: 1000, // Durée en millisecondes
         gravity: "top", // "top" ou "bottom"
-        position: "right", // "left", "center" ou "right"
+        position: "center", // "left", "center" ou "right"
         backgroundColor: "green",
     }).showToast();
 }
